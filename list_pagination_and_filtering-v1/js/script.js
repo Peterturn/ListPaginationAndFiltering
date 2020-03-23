6913c86feb673df7fcafad1294090e6193e34bee
Peterturn31 <55192090+Peterturn@users.noreply.github.com>
2020-03-23 17:52:30 +0100

    Unit 2 project day 1
    Unit 2 project day 1
    worked on Global  const
    
    hide/show function
    
    pagination buttons function.

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -2,49 +2,67 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-   
+
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
+/***
+   Add your global variables that store the DOM elements you will
+   need to reference and/or manipulate.
+
+   But be mindful of which variables should be global and which
+   should be locally scoped to one of the two main functions you're
+   going to create. A good general rule of thumb is if the variable
+   will only be used inside of a function, then it can be locally
    scoped to that function.
 ***/
 
+//Global variable(s)
+
+//Selects all studentItems and returns an array.
+const studentItems = document.querySelectorAll('li.student-item');
+//a const used to create groups of 10 students or less.
+const numOfBtnLinks = Math.ceil(studentItems.length/10);
+//console.log (numOfBtnLinks);
+const div = document.querySelector('div.page');
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
+//function to controll how many students are listed at the opening of the page.
+function showPage(text){
+  for (let i = 0; i < studentItems.length; i += 1) {
+    let li = studentItems[i];
+      if (i >= 9){
+      li.style.display = 'none';
+     }else {
+      li.style.display = li;
+      }
+    }
+}
+showPage(studentItems);
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
+/***
+   Create the `appendPageLinks function` to generate, append, and add
    functionality to the pagination buttons.
 ***/
 
 
+function appendPageLinks (){
+  const ul = document.createElement("ul");
+  div.appendChild(ul);
 
+  for (let i = 0; i <= numOfBtnLinks ; i++){
+    const li = document.createElement("li");
+    const anchor = document.createElement("a");
+    anchor.class = 'active';
+    li.innerHTML = parseInt([i])+1;
+    ul.appendChild(li);
+    li.appendChild(anchor);
+
+
+}
+}
+appendPageLinks ();
 
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
