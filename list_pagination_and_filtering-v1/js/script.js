3b52643256e0191e59f82e68167b8edb2097430a
Peterturn31 <55192090+Peterturn@users.noreply.github.com>
2020-03-24 15:51:23 +0100

    cleaned some code and reworked the pagination function.
    stuff

--- list_pagination_and_filtering-v1/js/script.js
+++ list_pagination_and_filtering-v1/js/script.js
@@ -6,17 +6,7 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/***
-   Add your global variables that store the DOM elements you will
-   need to reference and/or manipulate.
-
-   But be mindful of which variables should be global and which
-   should be locally scoped to one of the two main functions you're
-   going to create. A good general rule of thumb is if the variable
-   will only be used inside of a function, then it can be locally
-   scoped to that function.
-***/
-
+//Clean everything but notes and code.
 //Global variable(s)
 
 //Selects all studentItems and returns an array.
@@ -31,16 +21,16 @@ const mainBody = document.querySelector('body');
 //function to controll how many students are listed at the opening of the page.
 
 function showPage(num){
-  for (let i = num-1; i < studentItems.length; i += 1) {
+  for (let i = 0; i < studentItems.length; i += 1) {
     let li = studentItems[i];
-      if (i <= 9){
-      li.style.display = li;
-    }else if(){
-      li.style.display = 'none';
+      if (i <= (num-1)*10+9){
+      li.style.display = '';
+    }else if(i <= 9){
+      li.style.display = '';
       }
     }
 }
-showPage(1)
+showPage(1);
 
 /***
    Create the `appendPageLinks function` to generate, append, and add
@@ -72,15 +62,5 @@ const anchorDiv = document.querySelector('div.pagination');
 anchorDiv.addEventListener('click', (e) => {
   const anchorElement = e.target.textContent;
   const anchorNumber =  parseInt(anchorElement);
-  showPage(anchorNumber)
+  showPage(anchorNumber);
 });
-
-// for (let i = anchorNumber-1; i < studentItems.length; i += 1) {
-//   let li = studentItems[i];
-//   if (i <= i+9){
-//   li.style.display = li;
-//  }else {
-//   li.style.display = 'none';
-//   }
-// }
-// // Remember to delete the comments that came with this file, and replace them with your own code comments.
